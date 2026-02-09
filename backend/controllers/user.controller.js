f49c28adda41aa85780633b64079004c81af2fa9
2shining <fetrafaneva@gmail.com>
2026-02-09 20:06:30 +0300

    feat: Create userLogin API

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcrypt";
 
-const registerUser = async (req, res) => {
+export const registerUser = async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
@@ -51,4 +51,43 @@ const registerUser = async (req, res) => {
   }
 };
 
-export default registerUser;
+export const loginUser = async (req, res) => {
+  try {
+    const { identifier, password } = req.body;
+    // identifier = email OU username
+
+    if (!identifier?.trim() || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    // chercher l'utilisateur
+    const user = await User.findOne({
+      $or: [{ email: identifier.toLowerCase() }, { username: identifier }],
+    });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    // vérifier le mot de passe
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    res.status(200).json({
+      message: "Login successful",
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
